e81df8187d277e38755ad127b9eeb721e16d310d
Rendow <rendow33@gmail.com>
2021-03-24 21:08:16 +0300

    Updates

---
+++ precache-manifest.5919cac99c7da6ae9c3e1e07ee2a77c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5931d8ed4ac7931066da4912ad3f1a35",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "c5061776df16e35b50cb",
+    "url": "/TodoList/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "221330d7b0dad3343ec4",
+    "url": "/TodoList/static/js/2.c16d1b68.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/TodoList/static/js/2.c16d1b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5061776df16e35b50cb",
+    "url": "/TodoList/static/js/main.3c965ecb.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
